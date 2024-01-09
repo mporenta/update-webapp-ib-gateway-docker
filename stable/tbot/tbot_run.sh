c92a415c043b786d83f7d9aa4c961b2e40bc8926
Plusgenie Ltd <aladdin@plusgenie.com>
2024-01-09 15:46:36 +0000

    Enhance TradingBoat Docker Compatibility on Windows
    Resolved issues related to Docker container setup on Windows platforms.
    
    Issue:
    
    /home/tbot/tbot_run.sh: 10: /root/scripts/run_x11_vnc.sh: not found
    /home/tbot/tbot_run.sh: 15: /root/scripts/fork_ports_delayed.sh: not found

--- stable/tbot/tbot_run.sh
+++ stable/tbot/tbot_run.sh
@@ -5,14 +5,39 @@ export DISPLAY=:1
 rm -f /tmp/.X1-lock
 Xvfb :1 -ac -screen 0 1024x768x16 &
 
+
+echo "Attempting to execute /root/scripts/run_x11_vnc.sh"
+ls -l /root/scripts/run_x11_vnc.sh
+
+# Print the current working directory and the current user running the script
+echo "Current working directory: $(pwd)"
+echo "Current user: $(whoami)"
+
+# List files under /root/scripts
+echo "Listing files under /root/scripts:"
+ls -l /root/scripts
+
+echo "VNC_SERVER_PASSWORD is set to: $VNC_SERVER_PASSWORD"
+
+# Now proceed with the rest of the script
 if [ -n "$VNC_SERVER_PASSWORD" ]; then
   echo "Starting VNC server"
-  /root/scripts/run_x11_vnc.sh &
+  if [ -f "/root/scripts/run_x11_vnc.sh" ]; then
+    /root/scripts/run_x11_vnc.sh &
+  else
+    echo "ERROR: /root/scripts/run_x11_vnc.sh not found!"
+  fi
 fi
 
 envsubst < "${IBC_INI}.tmpl" > "${IBC_INI}"
 
-/root/scripts/fork_ports_delayed.sh &
+# Check if /root/scripts/fork_ports_delayed.sh exists before running it
+if [ -f "/root/scripts/fork_ports_delayed.sh" ]; then
+  echo "VNC: Forking ports"
+  /root/scripts/fork_ports_delayed.sh &
+else
+  echo "ERROR: /root/scripts/fork_ports_delayed.sh not found!"
+fi
 
 # TBOT: run ngrok, flask and tbot
 /home/tbot/develop/github/tbot-tradingboat/tbottmux/run_docker_flask_tbot.sh &
