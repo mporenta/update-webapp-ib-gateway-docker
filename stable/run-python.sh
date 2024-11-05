9431e83a0da95c51371b8f7382deeb9ad7734687
Jon Joe Joe <mike.porenta@gmail.com>
2024-11-04 21:02:02 -0700

    Update run-python.sh

--- stable/run-python.sh
+++ stable/run-python.sh
@@ -1,6 +1,3 @@
-
 #!/bin/sh
-sleep 60 
-python3 /home/tbot/develop/github/portfolio-monitor/src/run_pnl_monitor.py
 sleep 60
-python3 /home/tbot/develop/github/portfolio-monitor/src/pnl_web_service.py
+python3 ./src/pnl_monitor.py
